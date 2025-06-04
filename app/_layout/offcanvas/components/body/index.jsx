1c309f0b2a19b1663ddb8295abc00836b9b5b6c5
Beyond Hunger Admin <nealjain@beyond_hunger.com>
2025-06-05 00:28:31 +0530

    Enhance offcanvas close button visibility and functionality

--- app/_layout/offcanvas/components/body/index.jsx
+++ app/_layout/offcanvas/components/body/index.jsx
@@ -24,10 +24,13 @@ export function OffcanvasBody({ handleClose }) {
       {/* Content container with responsive width */}
       <div className='absolute right-0 top-0 h-screen w-full bg-foreground text-background overflow-y-auto md:w-2/3 lg:w-1/2'>
         <div className='flex h-full flex-col justify-between p-6 sm:p-12 md:p-16 lg:p-24'>
-          {/* Close button */}
-          <div className='absolute top-6 right-6 z-[600]'>
-            <MagneticButton onClick={handleClose} className='text-white'>
-              <X size={24} />
+          {/* Close button - Made larger and more prominent */}
+          <div className='fixed top-6 right-6 z-[600]'>
+            <MagneticButton 
+              onClick={handleClose} 
+              className='text-white bg-black p-3 rounded-full shadow-lg hover:bg-gray-800 transition-colors'
+            >
+              <X size={32} strokeWidth={2} />
             </MagneticButton>
           </div>
           <OffcanvasLinks />
